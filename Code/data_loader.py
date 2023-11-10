163fdb9b6866a207a727b0e6d9a4161b505eae49
kardin17 <sridevi.ksachar@gmail.com>
2023-11-10 13:53:07 -0500

    Code Updates

--- Code/data_loader.py
+++ Code/data_loader.py
@@ -1,12 +1,24 @@
 from torch.utils import data
+import SimpleITK as sitk
+from preprocess import readNpreprocessimage
 
 class BrainTumorSegDataset(data.Dataset):
-    def __init__(self):
-        pass
-    def __getitem__(self, index):
-        pass
-    def __len__(self):
-        pass
+    def __init__(self, imgs_pth, lbls_pth, config, mode):
+        self.config = config
+        self.mode = mode
+        self.imgs_pth = imgs_pth
+        self.lbls_pth = lbls_pth
 
-def get_loader(config, imgs, lbls, evaltype):
-    pass
+
+    def __getitem__(self, index):
+        img_pth = self.imgs_pth[index]
+        lbl_pth = self.lbls_pth[index]
+        images = readNpreprocessimage(img_pth, mask=False)
+        mask_lbl = readNpreprocessimage(lbl_pth, mask=True)
+        survival_lbl = lbl_pth['survivaldays']
+
+    def __len__(self):
+        return len(self.imgs_pth)
+
+def get_loader(config, imgs_pth, lbls_pth, mode):
+    dataset = BrainTumorSegDataset(imgs_pth, lbls_pth, config, mode)
--- Code/preprocess.py
+++ Code/preprocess.py
@@ -1,3 +1,11 @@
+import SimpleITK as sitk
+
+
+
+def readNpreprocessimage(imgs_pth, mask=False):
+    pass
+
+
 #Preprocessing Steps
 # 1) Histogram Equalization: https://simpleitk.readthedocs.io/en/master/link_SliceBySliceDecorator_docs.html
 # 2) Bias Field Correction: https://medium.com/@alexandro.ramr777/how-to-do-bias-field-correction-with-python-156b9d51dd79
